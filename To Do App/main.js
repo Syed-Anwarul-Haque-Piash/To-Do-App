3494e063a7e5be530984160ce36bc1ca779abaa0
Syed Anwarul Haque Piash <70095676+Syed-Anwarul-Haque-Piash@users.noreply.github.com>
2021-08-21 00:18:10 +0600

    Add files via upload

---
+++ To Do App/main.js
@@ -0,0 +1,17 @@
+//using loop for chnage all items
+// let itemUL = document.getElementById('items');
+// let items = itemUL.getElementsByClassName('item');
+// for (i = 0; i < items.length; i++) {
+//     items[i].style.color = 'red';
+// 
+
+//Query selsctor...Chnage last Item
+// let lastChild = document.querySelector('.item:last-child');
+// lastChild.style.color = 'blue'; 
+//query SelectorAll Change Both last 2 child
+
+// let lastItems = document.querySelectorAll('.item:last-child');
+
+// for (let element of lastItems) {
+//     element.style.color = 'red';
+// }
